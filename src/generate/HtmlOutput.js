dfc4e3d0f154633ab07ad56725df3d257bb2d59e
ValentinSii <vchristozov@gmail.com>
2022-06-29 11:03:03 +0200

    meta tag for encoding

--- src/generate/HtmlOutput.js
+++ src/generate/HtmlOutput.js
@@ -456,6 +456,7 @@ class htmlGenerator {
         <html>
         <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
         <link rel="stylesheet" href="https://unpkg.com/purecss@2.1.0/build/pure-min.css" integrity="sha384-yHIFVG6ClnONEA5yB5DJXfW2/KC173DIQrYoZMEtBvGzmf0PKiGyNEqe9N6BNDBH" crossorigin="anonymous">
         
 
